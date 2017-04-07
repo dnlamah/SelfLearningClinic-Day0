3e97de32289c5b8c175067b1fe88c8fa24f7e2ee
dnlamah <dnlamah1@gmail.com>
2017-04-07 22:43:51 +0100

    Add index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+function celciusToFar(F){
+
+	return (5/9)*(F-32)
+
+	
+}
+
+console.log(celciusToFar(10));
+
+
+function FarToCel(C){
+
+	return ((9/5) * C) + 32
+
+	
+}
+
+console.log(FarToCel(10));
+
+
+
+
+	function fibonaci(N)
+		for (i++ i<N; i=1;){
+
+			return i+(i-1)+(i-2);
+
+		}
+
+console.log(fibonaci(50));
